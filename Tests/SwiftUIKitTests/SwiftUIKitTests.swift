ab3905490f7ed8b0c896210fbde422fbbe425804
Steve Moon <steve@clevercoding.com>
2025-06-28 11:13:07 -0600

    chore(agent): commit all workspace and submodule changes before further workflow or refactor actions

--- Tests/SwiftUIKitTests/SwiftUIKitTests.swift
+++ Tests/SwiftUIKitTests/SwiftUIKitTests.swift
@@ -1,10 +1,17 @@
 import XCTest
+import SwiftUI
 @testable import SwiftUIKit
 
 final class SwiftUIKitTests: XCTestCase {
-    func testChatViewInstantiation() {
-        // This test simply checks that the ChatView can be instantiated
-        let view = ChatView()
+    func testChatHistoryViewInstantiation() {
+        struct TestMessage: Identifiable, Hashable {
+            let id = UUID()
+            let text: String
+        }
+        let messages = [TestMessage(text: "Hello"), TestMessage(text: "World!")]
+        let view = ChatHistoryView(messages: messages) { message in
+            AnyView(Text(message.text))
+        }
         XCTAssertNotNil(view)
     }
     // TODO: Add more comprehensive tests for all public components
